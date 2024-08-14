60b24499caf3fc3b6a9c8e1d7106d9b4d377d1c5
Palak <palak.desai4501@gmail.com>
2024-08-14 19:36:57 -0400

    LeetCode: 27. Remove Element

--- Arrays/removeElement.java
+++ Arrays/removeElement.java
@@ -16,7 +16,16 @@ package Arrays;
  */
 
 public class removeElement {
-    public static void main(String args[]) {
 
+    public int remove_Element(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+
+        }
+        return k;
     }
 }
