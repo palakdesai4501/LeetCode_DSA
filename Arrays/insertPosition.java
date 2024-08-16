b9b0eae5bf1a87bba1bcda3241aa7fb17153cdb6
Palak <palak.desai4501@gmail.com>
2024-08-16 19:21:40 -0400

    LeetCode: 35. Search Insert Position

---
+++ Arrays/insertPosition.java
@@ -0,0 +1,25 @@
+/*
+ * Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+ * If not, return the index where it would be if it were inserted in order.
+ */
+
+package Arrays;
+
+public class insertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
